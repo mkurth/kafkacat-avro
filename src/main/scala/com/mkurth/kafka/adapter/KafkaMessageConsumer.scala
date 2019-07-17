db776d1cee479eb0f07d0064a4d21df39058c919
Michael Kurth <michael.kurth@springernature.com>
2019-07-17 13:49:21 +0200

    made --until-date parameter actually work

--- src/main/scala/com/mkurth/kafka/adapter/KafkaMessageConsumer.scala
+++ src/main/scala/com/mkurth/kafka/adapter/KafkaMessageConsumer.scala
@@ -6,11 +6,10 @@ import java.util.Properties
 import java.util.concurrent.atomic.AtomicInteger
 
 import com.mkurth.kafka.domain.MessageConsumer
-import org.apache.kafka.clients.consumer.{ConsumerRecord, ConsumerRecords, KafkaConsumer, OffsetAndTimestamp}
+import org.apache.kafka.clients.consumer.{ConsumerRecord, ConsumerRecords, KafkaConsumer}
 import org.apache.kafka.common.{PartitionInfo, TopicPartition}
 import org.slf4j.{Logger, LoggerFactory}
 
-import scala.collection.mutable
 import scala.jdk.CollectionConverters._
 import scala.util.Try
 
@@ -29,22 +28,24 @@ class KafkaMessageConsumer(baseConfig: Config) extends MessageConsumer[Array[Byt
     val dateOffset     = new java.lang.Long(kafkaConfig.fromDate.toInstant.toEpochMilli)
     val consumerOffset = findOffsetsForDate(consumer, partitions, dateOffset)
     val latestOffsets  = kafkaConfig.untilDate.map(odt => findOffsetsForDate(consumer, partitions, new lang.Long(odt.toInstant.toEpochMilli)))
+    val offsetRanges   = calculateOffsetRanges(consumerOffset, latestOffsets)
 
-    consumer.assign(consumerOffset.keys.toList.asJava)
-    val noOffsetsFound = consumerOffset.forall(_._2 == null)
-    if (noOffsetsFound)
+    if (consumerOffset.isEmpty)
       logger.error("no offsets found for given date. try an earlier date.")
     else {
-      seekToOffset(consumer, consumerOffset)
-      val readSoFar = new AtomicInteger()
-      while (limitNotReached(kafkaConfig, readSoFar)) {
+      initKafkaClient(kafkaConfig, consumer)
+      seekToOffset(consumer, offsetRanges, kafkaConfig.topic)
+      val readSoFar   = new AtomicInteger()
+      var assignments = consumer.assignment().asScala.toList.map(_.partition())
+      while (limitNotReached(kafkaConfig, readSoFar) && assignments.nonEmpty) {
         val records: ConsumerRecords[Key, Value] = consumer.poll(defaultTimeout)
         records
           .iterator()
           .asScala
           .foreach(record => {
-            pausePartitionsThatReachedTheEnd(latestOffsets, consumer, record)
-            if (readSoFar.getAndIncrement() < kafkaConfig.limit) process(record.key(), record.value())
+            assignments = pausePartitionsThatReachedTheEnd(latestOffsets, consumer, record, kafkaConfig.topic, assignments)
+            if (readSoFar.getAndIncrement() < kafkaConfig.limit && recordInRange(offsetRanges, record))
+              process(record.key(), record.value())
           })
         consumer.commitSync()
       }
@@ -52,22 +53,47 @@ class KafkaMessageConsumer(baseConfig: Config) extends MessageConsumer[Array[Byt
     }
   }
 
+  private def initKafkaClient(kafkaConfig: KafkaConfig, consumer: KafkaConsumer[Key, Value]) = {
+    consumer.subscribe(List(kafkaConfig.topic).asJava)
+    Try(consumer.poll(Duration.ofSeconds(5)))
+  }
+
+  private def calculateOffsetRanges(consumerOffset: Map[Int, Long], latestOffsets: Option[Map[Int, Long]]): Map[Int, Range] =
+    latestOffsets
+      .map(off =>
+        off.map {
+          case (partition, offset) => (partition, Range(consumerOffset.getOrElse(partition, Long.MaxValue), offset))
+      })
+      .getOrElse(consumerOffset.map {
+        case (partition, offset) => (partition, Range(offset, Long.MaxValue))
+      })
+      .toMap
+
+  private def recordInRange(offsetRanges: Map[Int, Range], record: ConsumerRecord[Key, Value]) =
+    offsetRanges.get(record.partition()).exists(_.end >= record.offset())
+
   private def limitNotReached(kafkaConfig: KafkaConfig, readSoFar: AtomicInteger) =
     readSoFar.get() < kafkaConfig.limit
 
-  private def pausePartitionsThatReachedTheEnd(maxTimestamp: Option[mutable.Map[TopicPartition, OffsetAndTimestamp]],
+  private def pausePartitionsThatReachedTheEnd(maxTimestamp: Option[Map[Int, Long]],
                                                consumer: KafkaConsumer[Key, Value],
-                                               record: ConsumerRecord[Key, Value]): Unit =
+                                               record: ConsumerRecord[Key, Value],
+                                               topic: String,
+                                               assignments: List[Int]) =
     maxTimestamp
-      .flatMap(_.find(lastAllowedOffset => record.partition() == lastAllowedOffset._1.partition() && record.offset() > lastAllowedOffset._2.offset()))
-      .foreach(offset => Try(consumer.pause(List(offset._1).asJava)))
+      .flatMap(_.find(lastAllowedOffset => record.partition() == lastAllowedOffset._1 && record.offset() >= lastAllowedOffset._2))
+      .map(offset => {
+        Try(consumer.pause(List(new TopicPartition(topic, offset._1)).asJava))
+        assignments.filter(_ != offset._1)
+      })
+      .getOrElse(assignments)
 
-  private def seekToOffset(consumer: KafkaConsumer[Key, Value], consumerOffset: mutable.Map[TopicPartition, OffsetAndTimestamp]): Unit =
+  private def seekToOffset(consumer: KafkaConsumer[Key, Value], consumerOffset: Map[Int, Range], topic: String): Unit =
     consumerOffset
-      .filter(_._2 != null)
+      .filter(_._2.distance != 0)
       .foreach({
         case (partition, offset) =>
-          consumer.seek(partition, offset.offset())
+          consumer.seek(new TopicPartition(topic, partition), offset.start)
       })
 
   private def createConfig(kafkaConfig: KafkaConfig): Properties = {
@@ -80,9 +106,7 @@ class KafkaMessageConsumer(baseConfig: Config) extends MessageConsumer[Array[Byt
     config
   }
 
-  private def findOffsetsForDate(consumer: KafkaConsumer[Key, Value],
-                                 partitions: List[PartitionInfo],
-                                 dateOffset: lang.Long): mutable.Map[TopicPartition, OffsetAndTimestamp] =
+  private def findOffsetsForDate(consumer: KafkaConsumer[Key, Value], partitions: List[PartitionInfo], dateOffset: lang.Long): Map[Int, Long] =
     consumer
       .offsetsForTimes(
         partitions
@@ -94,4 +118,12 @@ class KafkaMessageConsumer(baseConfig: Config) extends MessageConsumer[Array[Byt
         defaultTimeout
       )
       .asScala
+      .toList
+      .filter(_._2 != null)
+      .map(tp => tp._1.partition() -> tp._2.offset())
+      .toMap
+}
+
+case class Range(start: Long, end: Long) {
+  def distance: Long = end - start
 }
